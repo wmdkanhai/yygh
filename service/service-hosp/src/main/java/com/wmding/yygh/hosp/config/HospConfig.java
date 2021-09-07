dbe36199804b8e49ec5ac364d30f4befc21f988d
wmding <>
2021-09-07 20:20:28 +0800

    fix: 解决分页问题，添加分页插件

---
+++ service/service-hosp/src/main/java/com/wmding/yygh/hosp/config/HospConfig.java
@@ -0,0 +1,25 @@
+package com.wmding.yygh.hosp.config;
+
+import com.baomidou.mybatisplus.extension.plugins.PaginationInterceptor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author 明月
+ * @version 1.0
+ * @date 9/6/21 9:40 PM
+ * @description:
+ */
+@Configuration
+public class HospConfig {
+
+    /**
+     * 分页插件
+     *
+     * @return
+     */
+    @Bean
+    public PaginationInterceptor paginationInterceptor() {
+        return new PaginationInterceptor();
+    }
+}
